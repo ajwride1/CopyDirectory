0ad82579e53230d08926dd9ea913eaaf0632b11d
Adam Wride <ajwride1@gmail.com>
2021-10-28 21:38:36 +0100

    new test created to make sure that we are testing when no directory is given

--- CopyDirectory.Tests/Functions/Source.cs
+++ CopyDirectory.Tests/Functions/Source.cs
@@ -26,5 +26,39 @@ namespace CopyDirectory.Tests.Functions
                 throw;
             }
         }
+
+        [TestMethod]
+        public void DirectoryNotGiven()
+        {
+            try
+            {
+                CopyDirectory.Functions.Source sourceDirectory = new CopyDirectory.Functions.Source(null);
+            }
+            catch (CopyDirectory.Functions.Exceptions.DirectoryNotGiven directoryNotGiven)
+            {
+                Console.WriteLine(directoryNotGiven.Message);
+
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(directoryNotGiven.Message));
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            try
+            {
+                CopyDirectory.Functions.Source sourceDirectory = new CopyDirectory.Functions.Source("");
+            }
+            catch (CopyDirectory.Functions.Exceptions.DirectoryNotGiven directoryNotGiven)
+            {
+                Console.WriteLine(directoryNotGiven.Message);
+
+                Assert.IsTrue(!string.IsNullOrWhiteSpace(directoryNotGiven.Message));
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
     }
 }
