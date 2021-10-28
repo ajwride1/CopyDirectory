85f579212199fff8dc0c1cdbee49d178a8b48188
Adam Wride <ajwride1@gmail.com>
2021-10-28 22:51:47 +0100

    basic functionality created for actually copying the source directory and all of it's subfiles to a given target directory

--- CopyDirectory.Functions/Source.cs
+++ CopyDirectory.Functions/Source.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using CopyDirectory.Functions.Exceptions;
 
 namespace CopyDirectory.Functions
@@ -9,7 +10,7 @@ namespace CopyDirectory.Functions
     public class Source
     {
         public DirectoryInfo Info { get; set; }
-        public FileInfo[] AllFiles { get; set; }
+        public FileInfo[] Files { get; set; }
         public List<Source> SubDirectories { get; set; }
 
         public Source(string directoryPath)
@@ -22,7 +23,7 @@ namespace CopyDirectory.Functions
 
             Info = new DirectoryInfo(directoryPath);
 
-            AllFiles = Info.GetFiles();
+            Files = Info.GetFiles();
 
             SubDirectories = new List<Source>();
 
@@ -33,6 +34,45 @@ namespace CopyDirectory.Functions
             foreach (string subDirectory in subDirectories)
             {
                 SubDirectories.Add(new Source(subDirectory));
+            }
+        }
+
+        public async Task Copy(string targetDirectory, bool overwrite = true)
+        {
+            if (Directory.Exists(targetDirectory) && overwrite)
+            {
+                Source targetDirectorySource = new Source(targetDirectory);
+
+                targetDirectorySource.Empty();
+            }
+
+            Directory.CreateDirectory(targetDirectory);
+
+            foreach (Source subDirectory in SubDirectories)
+            {
+                string newSubDirectory = subDirectory.Info.FullName.Replace(this.Info.FullName, targetDirectory);
+
+                await subDirectory.Copy(newSubDirectory);
+            }
+
+            foreach (FileInfo file in Files)
+            {
+                string newFileName = file.FullName.Replace(Info.FullName, targetDirectory);
+
+                File.Copy(file.FullName, newFileName, overwrite);
+            }
+        }
+
+        public void Empty()
+        {
+            foreach (Source subDirectory in SubDirectories)
+            {
+                subDirectory.Empty();
+            }
+
+            foreach (FileInfo fileInfo in Files)
+            {
+                File.Delete(fileInfo.FullName);
             }
         }
     }
--- CopyDirectory.Tests/Functions/Source.cs
+++ CopyDirectory.Tests/Functions/Source.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,6 +14,7 @@ namespace CopyDirectory.Tests.Functions
         private List<string> _SubDirectories = new List<string>();
         private List<string> _FileNames = new List<string>();
         private List<string> _LargeFiles = new List<string>();
+        private const string _TargetDirectory = @"C:\Scratch\CopyDirectory\TestingTarget";
 
         [TestInitialize]
         public void SetUpTestDirectories()
@@ -62,6 +64,12 @@ namespace CopyDirectory.Tests.Functions
                 int megabytes = multiplier * 1000000;
 
                 File.WriteAllBytes(largeFile, new byte[megabytes * 3]);
+            }
+
+            if (Directory.Exists(_TargetDirectory))
+            {
+                CopyDirectory.Functions.Source targetDirectory = new CopyDirectory.Functions.Source(_TargetDirectory);
+                targetDirectory.Empty();
             }
         }
 
@@ -134,7 +142,7 @@ namespace CopyDirectory.Tests.Functions
 
             foreach (string largeFile in _LargeFiles)
             {
-                Assert.IsTrue(sourceDirectory.AllFiles
+                Assert.IsTrue(sourceDirectory.Files
                     .Select(f => f.FullName)
                     .Contains(largeFile));
             }
@@ -143,10 +151,43 @@ namespace CopyDirectory.Tests.Functions
             {
                 foreach (CopyDirectory.Functions.Source sourceDirectorySubDirectory in sourceDirectory.SubDirectories)
                 {
-                    Assert.IsTrue(sourceDirectorySubDirectory.AllFiles
+                    Assert.IsTrue(sourceDirectorySubDirectory.Files
                         .Select(f => f.Name)
                         .Contains(fileName));
                 }
+            }
+        }
+
+        [TestMethod]
+        public void Copy()
+        {
+            CopyDirectory.Functions.Source sourceDirectory = new CopyDirectory.Functions.Source(_ValidDirectory);
+
+            sourceDirectory.Copy(_TargetDirectory);
+
+            Assert.IsTrue(Directory.Exists(_TargetDirectory));
+
+            foreach (string subDirectory in _SubDirectories)
+            {
+                string newSubDirectory = subDirectory.Replace(_ValidDirectory, _TargetDirectory);
+
+                Assert.IsTrue(Directory.Exists(newSubDirectory));
+
+                CopyDirectory.Functions.Source newSubDirectorySource = new CopyDirectory.Functions.Source(newSubDirectory);
+
+                foreach (string fileName in _FileNames)
+                {
+                    Assert.IsTrue(newSubDirectorySource.Files
+                        .Select(f => f.Name)
+                        .Contains(fileName));
+                }
+            }
+
+            foreach (string largeFile in _LargeFiles)
+            {
+                string newLargeFile = largeFile.Replace(_ValidDirectory, _TargetDirectory);
+
+                Assert.IsTrue(File.Exists(newLargeFile));
             }
         }
     }
